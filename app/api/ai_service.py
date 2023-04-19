3fbbcd2a4dd425d724fd018d831ebcdce1291d4c
Boxuan <boxuan.liu@gmail.com>
2023-04-19 14:20:54 +0200

    test db connection

--- app/api/ai_service.py
+++ app/api/ai_service.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.ai_model import ai_prediction_model
+import db_manager
 
 # add to APIRouter
 ai_service = APIRouter()
@@ -11,17 +12,22 @@ async def get_game_performance_model():
     return "aiservice is running"
 
 
-# get prediction values
-@ai_service.get("/predict/{game_id}/{knowledge_name}")
-async def predict_value(game_id: str, knowledge_name: str):
-    prediction_value = await get_recommendation(knowledge_name, game_id)
+@ai_service.get("/predict/")
+async def predict():
+    prediction_value = await db_manager.get_statement()
     return prediction_value
 
-
-async def get_recommendation(knowledge_name, game_id):
-    recommendation = ai_prediction_model.recommendation()
-    if recommendation:
-        return recommendation
-    else:
-        print("no recommendation found")
-        return []
+# # get prediction values
+# @ai_service.get("/predict/{game_id}/{knowledge_name}")
+# async def predict_value(game_id: str, knowledge_name: str):
+#     prediction_value = await get_recommendation(knowledge_name, game_id)
+#     return prediction_value
+#
+#
+# async def get_recommendation(knowledge_name, game_id):
+#     recommendation = ai_prediction_model.recommendation()
+#     if recommendation:
+#         return recommendation
+#     else:
+#         print("no recommendation found")
+#         return []
--- app/api/db_manager.py
+++ app/api/db_manager.py
@@ -1,6 +1,12 @@
-from app.api.db import db_XAPI
+from db import db_learningLocker
 
 
-# get game performance model
+# get XAPI statement
 async def get_statement():
-    return await db_XAPI["XAPI"].find().distinct("statement")
+    collection = db_learningLocker["statements"]
+
+    query = {"objectType": "Activity"}
+    projection = {"_id": 0, "verb": 1}
+    results = collection.find(query, projection)
+
+    return await results
