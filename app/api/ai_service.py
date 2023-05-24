07608f8fcfb40479969808cfa85293a782ff9ef9
Boxuan <boxuan.liu@gmail.com>
2023-05-24 16:22:38 +0200

    update ai_service

--- app/api/ai_service.py
+++ app/api/ai_service.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 import time
+import datetime
 
 from ai_model import ai_prediction_model
 from api import db_manager
@@ -14,17 +15,27 @@ ai_service = APIRouter()
 # get all game performance model data
 @ai_service.post("/")
 async def get_recommendation(lena: LENA):
-    statements = await db_manager.get_statements()
-    result = ai_prediction_model.recommendation(statements)
+    if lena.user_id:
+        user_id = int(lena.user_id)
+    ref_id = lena.ref_id_course
+    done_MLE = await db_manager.get_done_MLE(user_id)
+    df = ai_prediction_model.recommendation(ref_id, done_MLE)
+    recommendation = df["ref_id1"].tolist()
+    print(recommendation)
 
     emil = EMIL()
-    emil.MLE_ref_id = [1, 2, 3]
+    emil.MLE_ref_id = recommendation
     emil.recommendation_reason = "1"
-    emil.transmitted_at = time.time()
+
+    # concert timestamp format
+    timestamp = time.time()
+    dt = datetime.datetime.fromtimestamp(timestamp)
+    formatted_datetime = dt.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+    emil.transmitted_at = formatted_datetime
+
     return emil
 
 
 @ai_service.get("/predict/")
 async def predict():
-    prediction_value = await db_manager.get_statements()
-    return prediction_value
+    return
