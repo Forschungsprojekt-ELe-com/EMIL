5ede000b433f99af29dfd6cbe3ba63175ee10efb
Boxuan <boxuan.liu@gmail.com>
2023-05-24 16:22:11 +0200

    update get_done_MLE()

--- app/api/db_manager.py
+++ app/api/db_manager.py
@@ -1,8 +1,18 @@
 from api.db import db_XAPI
 import asyncio
 
+
 # get XAPI statement
-async def get_statements():
-    async for document in db_XAPI["statements"].find():
+async def get_done_MLE(user_id):
+    condition_complete = {"statement.verb.display.en-US": "completed"}
+    condition_user_id = {"statement.actor.account.name": user_id}
+    query = {'$and': [condition_complete, condition_user_id]}
+    filter_keys = {"_id": 0, "statement.object.id": 1}
+
+    done_MLE = []
+
+    async for document in db_XAPI["statements"].find(query, filter_keys):
         print(document)
-        break
+        done_MLE.append(document["statement"]["object"]["id"])
+
+    return done_MLE
