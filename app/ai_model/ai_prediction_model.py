f7d8d7b12d90c582d76c8791eea2d33cf5d8cc0e
Boxuan <boxuan.liu@gmail.com>
2023-05-24 16:22:54 +0200

    update ai_prediction_model.py

--- app/ai_model/ai_prediction_model.py
+++ app/ai_model/ai_prediction_model.py
@@ -1,8 +1,12 @@
 #from surprise import KNNWithMeans
 #from surprise import Reader, Dataset
 
-import pandas as pd
+from ai_model import RunModel
 
 
-def recommendation(statements):
-    return "recommendation"
+def recommendation(ref_id, done_MLE):
+    number_of_recommendation = 20
+    level = 1
+    done_MLE = [644, 633, 655]
+    recommendations, _ = RunModel.get_recommendations(ref_id, level, number_of_recommendation, done_MLE)
+    return recommendations
