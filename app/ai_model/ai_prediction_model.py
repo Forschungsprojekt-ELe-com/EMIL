033a36a21d1f3843305c5c77caeff56681fe888a
Boxuan <boxuan.liu@gmail.com>
2023-06-19 17:40:39 +0200

    update recommendation function

--- app/ai_model/ai_prediction_model.py
+++ app/ai_model/ai_prediction_model.py
@@ -7,6 +7,5 @@ from ai_model import RunModel
 def recommendation(ref_id, done_MLE):
     number_of_recommendation = 20
     level = 1
-    done_MLE = [644, 633, 655]
     recommendations, _ = RunModel.get_recommendations(ref_id, level, number_of_recommendation, done_MLE)
     return recommendations
