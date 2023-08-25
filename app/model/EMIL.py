6b594caf57349537141dec6854a801121e429418
Boxuan <boxuan.liu@gmail.com>
2023-08-25 13:49:15 +0200

    update EMIL response model

--- app/model/EMIL.py
+++ app/model/EMIL.py
@@ -1,4 +1,15 @@
-class EMIL:
+class Data:
     MLE_ref_id: list
     recommendation_reason: str
+
+
+class Meta:
     transmitted_at: str
+    status: str
+    error: str
+
+
+class EMIL:
+    def __init__(self):
+        self.meta = Meta()
+        self.data = Data()
