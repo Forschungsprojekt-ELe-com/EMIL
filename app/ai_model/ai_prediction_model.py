95bcd16d0fbb61c68003ed8572f9908b4bb7e24f
Boxuan <boxuan.liu@gmail.com>
2023-08-25 13:49:28 +0200

    update AI model

--- app/ai_model/ai_prediction_model.py
+++ app/ai_model/ai_prediction_model.py
@@ -6,6 +6,9 @@ from ai_model import RunModel
 
 def recommendation(ref_id, done_MLE):
     number_of_recommendation = 20
-    level = 1
-    recommendations, _ = RunModel.get_recommendations(ref_id, level, number_of_recommendation, done_MLE)
+    mediaPref = 1 #1--> Audio / 2-->Text / 3-->Video
+    item_name = 647 #ref_id of latest interaction
+    level = 2
+    done_MLE = [633, 655, 635, 626, 650, 671, 665, 668, 647]
+    recommendations, _ = RunModel.get_recommendations(item_name, mediaPref, level, number_of_recommendation, done_MLE)
     return recommendations
