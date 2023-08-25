d403e305860dfc4cb37ca1e56d993417a558a1b1
Boxuan <boxuan.liu@gmail.com>
2023-08-25 13:49:41 +0200

    update AI service

--- app/api/ai_service.py
+++ app/api/ai_service.py
@@ -13,18 +13,16 @@ ai_service = APIRouter()
 
 
 # Recommendation endpoint
-@ai_service.post("/")
-async def get_recommendation(lena: LENA):
-    if lena.user_id:
-        user_id = int(lena.user_id)
-    ref_id = lena.ref_id_course
+@ai_service.get("/{user_id}/{count_recommendation}")
+async def get_recommendation(user_id: int, count_recommendation: int):
+
     done_MLE = await db_manager.get_done_MLE(user_id)
-    df = ai_prediction_model.recommendation(ref_id, done_MLE)
+    df = ai_prediction_model.recommendation([], done_MLE)
     recommendation = df["ref_id1"].tolist()
 
     emil = EMIL()
-    emil.MLE_ref_id = recommendation
-    emil.recommendation_reason = "1"
+    emil.data.MLE_ref_id = recommendation
+    emil.data.recommendation_reason = "1"
 
     # concert timestamp format
     timestamp = time.time()
@@ -35,6 +33,6 @@ async def get_recommendation(lena: LENA):
     return emil
 
 
-@ai_service.get("/predict/")
-async def predict():
-    return
+@ai_service.get("/")
+async def hello():
+    return "the server is running"
