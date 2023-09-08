b511b02d3d07b5c44c4b43b333841a1e41452184
Boxuan <boxuan.liu@gmail.com>
2023-09-08 12:05:22 +0200

    replace dummy data with parameter argument

--- app/ai_model/ai_prediction_model.py
+++ app/ai_model/ai_prediction_model.py
@@ -3,12 +3,11 @@
 
 from ai_model import RunModel
 
-
-def recommendation(ref_id, done_MLE):
+def recommendation(done_MLE):
     number_of_recommendation = 20
     mediaPref = 1 #1--> Audio / 2-->Text / 3-->Video
-    item_name = 647 #ref_id of latest interaction
     level = 2
-    done_MLE = [633, 655, 635, 626, 650, 671, 665, 668, 647]
+    item_name = done_MLE.pop()
+
     recommendations, _ = RunModel.get_recommendations(item_name, mediaPref, level, number_of_recommendation, done_MLE)
     return recommendations
