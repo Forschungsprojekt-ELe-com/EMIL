5aa48ddc8f3533cc9910d21496c7ed23e706f449
Boxuan <boxuan.liu@gmail.com>
2023-09-08 12:03:22 +0200

    update statement filtering conditions

--- app/api/db_manager.py
+++ app/api/db_manager.py
@@ -1,17 +1,26 @@
 from api.db import db_XAPI
 from urllib.parse import urlparse, parse_qs
+import re
 
 
 # get XAPI statement
 async def get_done_MLE(user_id):
-    condition_complete = {"statement.verb.display.en-US": "completed"}
-    condition_user_id = {"statement.actor.account.name": user_id}
-    query = {'$and': [condition_complete, condition_user_id]}
+    condition_de_complete = {"statement.verb.display.de-DE": "completed"}
+    condition_en_complete = {"statement.verb.display.en-US": "completed"}
+    condition_user_id = {"statement.actor.account.name": re.compile(f"^{user_id}")}
+    query = {
+        '$and': [
+            {
+                '$or': [condition_de_complete, condition_en_complete]
+            },
+            condition_user_id
+        ]
+    }
     filter_keys = {"_id": 0, "statement.object.id": 1}
 
     done_MLE = []
-
-    async for document in db_XAPI["statements"].find(query, filter_keys):
+    result = db_XAPI["statements"].find(query, filter_keys).sort([['_id', -1]])
+    async for document in result:
         ref_id = extract_ref_id(document["statement"]["object"]["id"])
         done_MLE.append(ref_id)
     return done_MLE
