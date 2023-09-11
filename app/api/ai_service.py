5f94bc5151193892960d395ab01063b61dee2a90
Boxuan <boxuan.liu@gmail.com>
2023-09-11 12:09:17 +0200

    change recommendation reason

--- app/api/ai_service.py
+++ app/api/ai_service.py
@@ -21,13 +21,14 @@ async def get_recommendation(user_id: int, count_recommendation: int):
     done_MLE = await db_manager.get_done_MLE(user_id)
     if not done_MLE:
         emil.meta.error = "user_id not found or no user data"
+        emil.data.recommendation_reason = "Aufgrund Ihrer aktuellen Einstellungen schlage ich Ihnen folgendes vor:"
     else:
         df = ai_prediction_model.recommendation(done_MLE)
         recommendation = df["obj_id1"].tolist()
+        emil.data.recommendation_reason = "Abgestimmt auf Ihre bisherige Auswahl schage ich vor:"
 
     filtered_recommendation = recommendation[:count_recommendation]
     emil.data.MLE_ref_id = filtered_recommendation
-    emil.data.recommendation_reason = "Abgestimmt auf Ihre bisherige Auswahl schage ich vor:"
 
     # convert timestamp format
     timestamp = time.time()
