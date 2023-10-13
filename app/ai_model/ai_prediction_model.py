c2e2e36ef8d01fc581811204f423eeca02ab1c46
Boxuan <boxuan.liu@gmail.com>
2023-10-13 12:22:26 +0200

    implement user_preference function

--- app/ai_model/ai_prediction_model.py
+++ app/ai_model/ai_prediction_model.py
@@ -3,11 +3,13 @@
 
 from ai_model import RunModel
 
-def recommendation(done_MLE):
+def recommendation(done_MLE, preference):
     number_of_recommendation = 20
-    mediaPref = 1 #1--> Audio / 2-->Text / 3-->Video
-    level = 2
+    media_pref = (preference[0] or 0)+1 #1--> Audio / 2-->Text / 3-->Video
+    level = (preference[1] or 0)+1
+
+    print(media_pref, level)
 
     # Get recommendations using AI model
-    recommendations, _ = RunModel.get_recommendations(mediaPref, level, number_of_recommendation, done_MLE)
+    recommendations, _ = RunModel.get_recommendations(media_pref, level, number_of_recommendation, done_MLE)
     return recommendations
--- app/api/ai_service.py
+++ app/api/ai_service.py
@@ -1,20 +1,27 @@
-from fastapi import APIRouter
+from fastapi import Depends, APIRouter
 import time
 import datetime
 
 from ai_model import ai_prediction_model
 from api import db_manager
-from model.LENA import LENA
 from model.EMIL import EMIL
-
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from typing_extensions import Annotated
 
 # add to APIRouter
 ai_service = APIRouter()
 
+# Add HTTP Basic Auth
+security = HTTPBasic()
 
 # Recommendation endpoint
 @ai_service.get("/{user_id}/{count_recommendation}")
+# async def get_recommendation(user_id: int, count_recommendation: int, credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
 async def get_recommendation(user_id: int, count_recommendation: int):
+    # check username and password
+    # if not credentials.username == xxx or not credentials.password == xxx:
+    #     return
+
     # Create Emil object
     emil = EMIL()
     recommendation = []
@@ -23,12 +30,13 @@ async def get_recommendation(user_id: int, count_recommendation: int):
     done_MLE = await db_manager.get_done_MLE(user_id)
     # Check if user exist in LRS
     user_exist = await db_manager.check_user(user_id)
+    user_preference = await db_manager.get_preference(user_id)
 
     # If there are obj_ids in done_MLE list, get recommendation using AI model
     if done_MLE:
-        df = ai_prediction_model.recommendation(done_MLE)
-        recommendation = df["obj_id1"].tolist()
-        emil.data.recommendation_reason = "Abgestimmt auf Ihre bisherige Auswahl schage ich vor:"
+        df = ai_prediction_model.recommendation(done_MLE, user_preference)
+        recommendation = df["obj_id3"].dropna().tolist()
+        emil.data.recommendation_reason = "Abgestimmt auf Ihre bisherige Auswahl schlage ich vor:"
 
     # If there is no obj_ids, check if the user_id exist in the LRS.
     elif user_exist:
