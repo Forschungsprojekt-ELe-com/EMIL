72b35cf531573b98585415d97b701aaf53b1751a
Boxuan <boxuan.liu@gmail.com>
2023-10-13 12:21:49 +0200

    add get_preference method

--- app/api/db_manager.py
+++ app/api/db_manager.py
@@ -3,6 +3,8 @@ from urllib.parse import urlparse, parse_qs
 import re
 
 
+TEST_OBJ_ID = "https://elecom.qualitus.net/goto.php?target=copa_1649&client_id=elecom&h5p_object_id=246&h5p-subContentId=118704d2-f551-4e1e-9f27-752e526dd12d&obj_id_lrs=1813"
+
 # get XAPI statement using query
 async def get_done_MLE(user_id):
     condition_de_complete = {"statement.verb.display.de-DE": "completed"}
@@ -39,6 +41,33 @@ async def check_user(user_id):
         obj_id = extract_obj_id(document["statement"]["object"]["id"])
         attempted_MLE.append(int(obj_id[0]))
     return attempted_MLE
+
+
+async def get_preference(user_id):
+
+    test_obj_id = 1813
+
+    # Condition
+    condition_user_id = {"statement.actor.account.name": re.compile(f"^{user_id}")}
+    condition_en_answered = {"statement.verb.display.en-US": "answered"}
+    condition_test_obj = {"statement.object.id": re.compile(f"{test_obj_id}$")}
+
+    query = {
+        '$and': [
+            condition_user_id, condition_en_answered, condition_test_obj
+        ]
+    }
+
+    # Filter result
+    filter_keys = {"_id": 0, "statement.result.response": 1}
+
+    preference = []
+
+    result = db_XAPI["statements"].find(query, filter_keys)
+    async for document in result:
+        obj_id = document["statement"]["result"]["response"]
+        preference.append(int(obj_id[0]))
+    return preference
 
 
 def extract_obj_id(url):
